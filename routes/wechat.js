c0254672fc57b48d12c5caa339df2bf4da18bd01
Jason.Zhang <lefttjs@gmail.com>
2016-06-16 08:16:59 +0800

    add

--- routes/wechat.js
+++ routes/wechat.js
@@ -17,7 +17,7 @@ var client = new OAuth(app_id, app_secret);
 // 主页,主要是负责OAuth认证
 router.get('/', function(req, res) {
     console.log(domain);
-    var url = client.getAuthorizeURL(domain + '/weixin/callback','','snsapi_userinfo');
+    var url = client.getAuthorizeURLForWebsite(domain + '/weixin/callback');
     res.redirect(url)
 })
 
@@ -32,54 +32,22 @@ router.get('/callback', function(req, res) {
     console.log('----weixin callback -----')
     var code = req.query.code;
 
-    var User = req.model.UserModel;
+
 
     client.getAccessToken(code, function (err, result) {
-        console.dir(err)
-        console.dir(result)
+        console.dir('err',err)
+        console.dir('result', result)
         var accessToken = result.data.access_token;
         var openid = result.data.openid;
 
         console.log('token=' + accessToken);
         console.log('openid=' + openid);
+        client.getUser(openId, function(err,result){
+    			if(err) return next(err)
+    			console.log('result', result)
+    			res.json(result)
+		      })
 
-        User.find_by_openid(openid, function(err, user){
-            console.log('微信回调后，User.find_by_openid(openid) 返回的user = ' + user)
-            if(err || user == null){
-                console.log('user is not exist.')
-                client.getUser(openid, function (err, result) {
-                    console.log('use weixin api get user: '+ err)
-                    console.log(result)
-                    var oauth_user = result;
-
-                    var _user = new User(oauth_user);
-                    _user.username = oauth_user.nickname;
-                    _user.nickname = oauth_user.nickname;
-
-                    _user.save(function(err, user) {
-                        if (err) {
-                            console.log('User save error ....' + err);
-                        } else {
-                            console.log('User save sucess ....' + err);
-                            req.session.current_user = void 0;
-                            res.redirect('/user/' + user._id + '/verify');
-                        }
-                    });
-
-                });
-            }else{
-                console.log('根据openid查询，用户已经存在')
-                // if phone_number exist,go home page
-                if(user.is_valid == true){
-                    req.session.current_user = user;
-                    res.redirect('/mobile')
-                }else{
-                    //if phone_number exist,go to user detail page to fill it
-                    req.session.current_user = void 0;
-                    res.redirect('/users/' + user._id + '/verify');
-                }
-            }
-        });
     });
 });
 
